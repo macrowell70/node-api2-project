edbc46dea025eb9abba4d961152e2ffb2e632701
unknown <macrowell@yahoo.com>
2022-06-07 19:07:06 -0400

    set up get endpoint for posts with specified ID

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -16,4 +16,22 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+    Posts.findById(req.params.id)
+    .then(result => {
+        if (!result) {
+            res.status(404).json({ 
+                message: "The post with the specified ID does not exist"
+             })
+             return;
+        };
+        res.json(result);
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The post information could not be retrieved"
+        })
+    })
+})
+
 module.exports = router;
